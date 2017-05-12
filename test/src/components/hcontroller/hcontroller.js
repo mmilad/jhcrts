74c52f328dee56468f1cf76b573c9df9cdf6b996
Meister <Meister>
2017-05-12 03:05:08 +0200

    major update

--- test/src/components/hcontroller/hcontroller.js
+++ test/src/components/hcontroller/hcontroller.js
@@ -1,34 +1,73 @@
 J.H().register({
     "h-controller" : {
         onSet: function(elem) {
+                var newElem = {
+                    class:"foo goo fete",
+                    children:[
+                        {
+                            class: "goo",
+                            html: "hello world"
+                        }
+                    ]
+                };
+                J.H(newElem);
+                elem.appendChild(newElem.element)
             elem.data.onSet.push(function(e){
                 console.log(e);
             });
-            elem.data = {
-                q: {
-                    w: {
-                        e: {
-                            r:{
-                                d:""
-                            }
-                        }
-                    }
-                },
-                a:{
-                    s:{
-                        d:{
-                            f:{
-                                g:""
-                            }
-                        }
-                    }
-                }
-            };
-            elem.data.q.w.e.r.d;
-            elem.data.a.s.d.f.g;
-            debugger
-            elem.data="";
-            elem.innerHTML = "HELLO WORLD! im home controller";
         }
     }
 })
+var styleObj = {
+    ".foo" : {
+        border: "1px solid black",
+        "background-color": "red",
+        "animation-name": "milad",
+        "animation-duration": "1s",
+        backgroundColor: "green",
+        ".gootte" : {
+            border: "1px solid green",
+        },
+        "&.goo": {
+            float:"left",
+            padding: "5px",
+            "&.fete": {
+                padding: "50px"
+            }
+        },
+        ".goo": {
+            float:"left",
+            padding: "5px",
+            "&.fete": {
+                padding: "50px"
+            }
+        }
+    }
+};
+J.C(styleObj);
+var myKeyFrame = {
+    "@Keyframes milad": {
+        to: {
+            padding: "150px",
+            "background-color": "red"
+        }
+    }
+}
+J.C({
+    ".foo" : {
+        "&:hover": {
+            "background-color": "yellow !important"
+        }
+    }
+});
+setTimeout(function() {
+J.C({
+    ".foo" : {
+        "&:hover": {
+            "background-color": "blue"
+        }
+    }
+});
+},3000)
+J.C(myKeyFrame);
+J.C().getRule('.foo:hover').border = "10px solid purple";
