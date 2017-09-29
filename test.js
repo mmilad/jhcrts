4ee94c0d41db0ac37a5a5b657bce5ba9d66badf8
mmilad <meistermilad.a@gmail.com>
2017-09-30 01:43:05 +0200

    saved

--- test.js
+++ test.js
@@ -1,21 +1,60 @@
-var himalaya = require('himalaya')
-var html = require('fs').readFileSync('./test.html', { encoding: 'utf8' })
+var himalaya = require('himalaya'),
+    fs = require('fs')
+var html = fs.readFileSync('./test.html', { encoding: 'utf8' })
+
+var component = {}
 
 function compileHtml(html) {
     html = html.replace(/\n/g, '');
     html = html.replace(/\r/g, '');
     html = html.replace(/\t/g, '');
     html = html.replace(/  /g, '');
+    
     var json = himalaya.parse(html)
-// console.log(json)
- console.dir(json, { depth: null })
-    json.forEach(readNode)
+ 
+    if(json[0].tagName === "component") {
+        if(json[0].attributes) {
+            if(!component[json[0].attributes.name]) {
+                component[json[0].attributes.name] = {}
+            }
+            component[json[0].attributes.name].tpl = []
+            if(json[0].children) {
+                json[0].children.forEach(e => {
+                    formatToConfig(e, component[json[0].attributes.name].tpl)
+                })
+            }
+            console.log( component, { depth: null })
+
+            saveConfig(component)
+        }
+    }
 }
 
 compileHtml(html)
-function readNode(node) {
-    if (node.tagName) console.log(node.tagName)
-    if(node.children) node.children.forEach(readNode);
+function formatToConfig(node, parent) {
+    var config = {
+        attributes: {},
+        children: []
+    }
+    if (node.tagName) {
+        config.tag = node.tagName;
+    }
+    if(node.children) {
+        node.children.forEach(e => {
+            formatToConfig(e, config.children)
+        });
+    }
+    parent.push(config)
 }
 // console.log(html)
 // console.dir(json, { depth: null })
+
+function saveConfig (str) {
+    fs.writeFile("/renderedConfig.js", str, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    
+        console.log("The file was saved!");
+    }); 
+}
